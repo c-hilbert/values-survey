487f1b23dbd1b73a181d062a977fa72e72dbdfaf
Carmen Hilbert <carminaburana512@gmail.com>
2021-10-01 17:05:35 -0400

    extraneous experiments deleted

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const SRC_DIR = path.join(__dirname, 'client');
-const OUT_DIR = path.join(__dirname, 'build');
+const OUT_DIR = path.join(__dirname, 'public');
 
 module.exports = {
   entry: path.join(SRC_DIR, 'index.jsx'),
